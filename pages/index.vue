54eba3c14ba7f017a64641d8b58c3630ac0d6396
emiraliyev235 <emiraliyev235@gmail.com>
2025-03-04 18:22:35 +0400

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
+<template>
+  <div>
+    <Head>
+      <title>INFG Pay</title>
+      <meta name="description" content="Процессинг INFG PAY" />
+    </Head>
+    <HomeView />
+  </div>
+</template>
+
 <script setup>
 import HomeView from '@/views/home/index.vue'
 </script>
-
-<template>
-  <HomeView />
-</template>
